d093969ea72b97eb1468144879787f554a32c75a
Andrei Bintintan <andy@interpid.eu>
2018-11-27 15:43:20 +0100

    #1701 - CI Integration

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,6 +8,8 @@ $_SERVER[ 'ENVIRONMENT' ] = 'test';
 
 define( 'PDF_RESOURCES_IMAGES', APPLICATION_PATH . '/content/images' );
 
+date_default_timezone_set( "Europe/Zurich" );
+
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
