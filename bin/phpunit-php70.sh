3a02f404ec7ae33e82952830baf789b36e8413f9
Andrei Bintintan <andy@interpid.eu>
2022-04-27 00:08:47 +0200

    Drop support for PHP5

--- bin/phpunit-php70.sh
+++ bin/phpunit-php70.sh
@@ -2,5 +2,5 @@ rm -r ./vendor/php*
 rm -r ./vendor/nikic/
 rm -r ./vendor/symfony/
 composer dump-autoload
-wget -O phpunit.phar https://phar.phpunit.de/phpunit-6.phar
+wget --no-check-certificate -O phpunit.phar https://phar.phpunit.de/phpunit-6.phar
 php phpunit.phar --bootstrap ./tests/bootstrap.php ./tests/functional
