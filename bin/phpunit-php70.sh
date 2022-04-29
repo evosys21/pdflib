5c9618777e42c60ad1086c69aca5d974cb46b5c5
Andrei Bintintan <andy@interpid.eu>
2022-04-29 17:40:26 +0200

    Update CI integration

--- bin/phpunit-php70.sh
+++ bin/phpunit-php70.sh
@@ -3,4 +3,4 @@ rm -r ./vendor/nikic/
 rm -r ./vendor/symfony/
 composer dump-autoload
 wget --no-check-certificate -O phpunit.phar https://phar.phpunit.de/phpunit-6.phar
-php phpunit.phar --bootstrap ./tests/bootstrap.php ./tests/functional
+php phpunit.phar --bootstrap ./tests/bootstrap.php ./tests/Functional
